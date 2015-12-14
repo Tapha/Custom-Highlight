b6646f7e4c57d3af8b267f5ca4aee9cd64490f85
Tapha Ngum <tapha@live.co.uk>
2015-12-14 13:30:20 +0000

    testing

---
+++ custom-highlight.js
@@ -0,0 +1,44 @@
+function getSelectedText() {
+  t = (document.all) ? document.selection.createRange().text : document.getSelection();
+
+  return t;
+}
+
+var mouseX;
+var mouseY;
+
+// use the mouse coordinates to set button position
+jQuery(document).mousemove(function (e) {
+  mouseX = e.pageX;
+  mouseY = e.pageY - 10;
+});
+
+function add_cs_class(s_text) {
+  var selection = s_text;
+    var selection_text = selection.toString();
+    
+    // How do I add a span around the selected text?
+    
+    var span = document.createElement('SPAN');
+    span.textContent = selection_text;
+    span.classList.add("cs-editing");
+  
+    var range = selection.getRangeAt(0);
+    range.deleteContents();
+    range.insertNode(span);
+  
+    var custom_hash_1 = Math.random().toString(36).slice(2);
+    span.id = "cs-editing-" + custom_hash_1;
+    $(span.id).tooltipster('show');
+    return span.id;
+}
+
+jQuery(document).ready(function ($) {
+  
+  $('div,p').mouseup(function (e) {
+    if (getSelectedText() != "") {
+      curr_span_id = add_cs_class(getSelectedText());
+      console.log(curr_span_id);
+    }
+  });
+});
